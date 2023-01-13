95f6b351dda1661618ff1afc1a76efd9dc7d8631
GladysKigen <gladys.kigen@student.moringa.com>
2023-01-13 16:01:27 +0300

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Header({name}) {
-    return <div> 
-        <h1> {name}</h1>
-    </div>
+    return (
+    <header> 
+         <h1> {name}</h1>
+    </header>
+    );
 };
 
 export default Header;
